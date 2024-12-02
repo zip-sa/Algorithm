127c740bc9a2fb94c68ca046c63e28a9a6495565
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-02 19:32:05 +0900

    Title: 비교 연산, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241202/비교 연산/comparison-operator.py
@@ -0,0 +1,23 @@
+a, b = map(int, input().split())
+
+if a>=b: print("1")
+else: print("0")
+
+if a>b: print("1")
+else: print("0")
+
+if b>=a: print("1")
+else: print("0")
+
+if b>a: print("1")
+else: print("0")
+
+if a==b: print("1")
+else: print("0")
+
+if a!=b: print("1")
+else: print("0")
+
+
+
+# print(a>=b, a>b, b>=a, b>a, a==b, a!=b, sep="\n")
