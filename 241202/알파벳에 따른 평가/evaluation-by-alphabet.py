b37d7aa1e63bf0a8ce4b41b36359ce69cf2c126a
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-02 18:00:10 +0900

    Title: 알파벳에 따른 평가, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241202/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,8 @@
+s = input()
+
+if s == 'S': print("Superior")
+elif s == 'A': print("Excellent")
+elif s == 'B': print("Good")
+elif s == 'C': print("Usually")
+elif s == 'D': print("Effort")
+else: print("Failure")
