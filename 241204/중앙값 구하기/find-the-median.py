db68da5a87599b74a3f46f815bf6b1b30138b9d8
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-04 01:50:58 +0900

    Title: 중앙값 구하기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241204/중앙값 구하기/find-the-median.py
+++ 241204/중앙값 구하기/find-the-median.py
@@ -4,3 +4,25 @@ n = len(arr)
 arr_sorted = sorted(arr)
 
 print((arr_sorted[n//2] + arr_sorted[n//2-1])/2 if n%2==0 else arr_sorted[n//2])
+
+# # 변수 선언 및 입력
+# a, b, c = map(int, input().split())
+# 
+# if a > b:
+#     if c > a:
+#         # a > b, c > a 일때 (c > a > b)
+#         print(a)
+#     # a > b, a > c 일때 (a가 가장 크고, b와 c중 더 큰 수가 중앙값)
+#     elif b > c:
+#         print(b)
+#     else:
+#         print(c)
+# else:
+#     if c > b:
+#         # b > a, c > b 일때 (c > b > a)
+#         print(b)
+#     # b > a, b > c 일때 (b가 가장 크고, a와 c중 더 큰 수가 중앙값)
+#     elif a > c:
+#         print(a)
+#     else:
+#         print(c)
