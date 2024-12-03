7b729fa6ae5ae536b48ed72b980afb26815769cc
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-03 20:59:47 +0900

    Title: 최댓값 구하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241203/최댓값 구하기/maximum-value.py
@@ -0,0 +1,17 @@
+arr = list(map(int, input().split()))
+
+print(max(arr)) 
+
+# a, b, c = map(int,input().split())
+# 
+# if a > b:
+#     if a >= c:
+#         print(a)
+#     else:
+#         print(c)
+# 
+# if a <= b:
+#     if b >= c:
+#         print(b)
+#     else:
+#         print(c)
