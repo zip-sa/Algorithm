f4d46d4d02008dea58bf7ac32dd51170975d1cf5
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-03 21:10:24 +0900

    Title: 코로나 메뉴얼, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241203/코로나 메뉴얼/covid-manual.py
+++ 241203/코로나 메뉴얼/covid-manual.py
@@ -13,3 +13,25 @@ if count >= 2:
     print("E")
 else:
     print("N")
+
+# # 변수 선언 및 입력
+# inp = input().split()
+# c1, t1 = inp[0], int(inp[1])
+# inp = input().split()
+# c2, t2 = inp[0], int(inp[1])
+# inp = input().split()
+# c3, t3 = inp[0], int(inp[1])
+# 
+# # A가 2명 이상인지 판단하기
+# if c1 == 'Y' and t1 >= 37:
+#     # 첫 번째 사람이 A라면, 남은 두 사람 중 한 사람이라도 A면 됩니다.
+#     if (c2 == 'Y' and t2 >= 37) or (c3 == 'Y' and t3 >= 37):
+#         print("E")
+#     else:
+#         print("N")
+# else:
+#     # 첫 번째 사람이 A가 아니라면, 남은 두 사람 모두 A여야만 합니다.
+#     if (c2 == 'Y' and t2 >= 37) and (c3 == 'Y' and t3 >= 37):
+#         print("E")
+#     else:
+#         print("N")
