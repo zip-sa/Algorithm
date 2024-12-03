3d1b714279c4d60e255debe87202b2816e3f81f2
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-03 18:25:41 +0900

    Title: 장학금, Time: 76ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241203/장학금/scholarship.py
+++ 241203/장학금/scholarship.py
@@ -1,9 +1,8 @@
 mid, final = map(int,input().split())
 
-if mid >= 90:
-    if final >= 95:
-        print(100000)
-    elif final >= 90:
-        print(50000)
+if mid >= 90 and fianl >= 95:
+    print(100000)
+elif mid >= 90 and final >= 90:
+    print(50000)
 else:
     print(0)
