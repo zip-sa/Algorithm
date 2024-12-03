8fe94e1966fe842a6d5a846d92cbd49817754ae4
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-03 18:24:17 +0900

    Title: 장학금, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241203/장학금/scholarship.py
+++ 241203/장학금/scholarship.py
@@ -2,8 +2,8 @@ mid, final = map(int,input().split())
 
 if mid >= 90:
     if final >= 95:
-        print(10)
+        print(100000)
     elif final >= 90:
-        print(5)
+        print(50000)
 else:
     print(0)
