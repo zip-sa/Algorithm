dcdf2eaed60ee1e4cc3f3b63285b5ea5e3d4e661
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-07 14:37:14 +0900

    Title: a 부터 b 까지, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241207/a 부터 b 까지/a-to-b.py
+++ 241207/a 부터 b 까지/a-to-b.py
@@ -1,12 +1,11 @@
 a, b = map(int, input().split())
 print(a, end=' ')
 
-while a < b and (a*2 < b or a+2 < b):
+while a < b:
     if a % 2 != 0:
         a *= 2
-        print(a, end=' ')
-
-    elif a % 3 != 0:
+    else:
         a += 3
-        print(a, end=' ')
-    
+    if a > b:
+        break
+    print(a, end=' ')
