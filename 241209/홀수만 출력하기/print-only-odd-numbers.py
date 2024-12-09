1ddfa1379c3ee6b4105920cad040adf14886d994
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-09 21:57:55 +0900

    Title: 홀수만 출력하기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241209/홀수만 출력하기/print-only-odd-numbers.py
+++ 241209/홀수만 출력하기/print-only-odd-numbers.py
@@ -1,12 +1,23 @@
 n = int(input())
 arr = []
-k = 0
 
 for _ in range(n):
     num = int(input())
     arr.append(num)
 
-for _ in range(n):
-    if arr[k] % 2 != 0 and arr[k] % 3 == 0:
-        print(int(arr[k]))
-    k += 1
+for num in arr:
+    if num % 2 != 0 and num % 3 == 0:  # 홀수이면서 3의 배수인 경우
+        print(num)
+
+# n = int(input())
+# arr = []
+# k = 0
+# 
+# for _ in range(n):
+#     num = int(input())
+#     arr.append(num)
+# 
+# for _ in range(n):
+#     if arr[k] % 2 != 0 and arr[k] % 3 == 0:
+#         print(int(arr[k]))
+#     k += 1
