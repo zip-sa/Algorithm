0790e02947c9c6dd395c307a76889b65a8eacc6a
Seungwoo Park <49730616+zip-sa@users.noreply.github.com>
2024-12-18 01:47:55 +0900

    Title: 출석 부르기 2, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241218/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,10 @@
+while True:
+    n = int(input())
+    if n == 1: print("John")
+    elif n == 2: print("Tom")
+    elif n == 3: print("Paul")
+    elif n == 4: print("Sam")
+    else:
+        print("Vacancy")
+        break
+        
